bd72f0e4e72e72b68c47d1b3a56ff90775553a66
iremgaltun <21060625@stu.omu.edu.tr>
2023-10-26 21:17:19 +0300

    güncelleme

--- ödev/hesapmakinesi.dart
+++ ödev/hesapmakinesi.dart
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 
 import 'dart:io';
 
-=======
-import 'dart:ffi';
-import 'dart:io';
-
->>>>>>> ded766f1229b090f3db6c06c86ae5c2d407f400c
 
 
 void main(List<String> args) {
@@ -14,14 +8,11 @@ void main(List<String> args) {
  while(true)
     anasayfa();
 
-<<<<<<< HEAD
 }
 
   anasayfa(){
     print("---HESAP MAKİNESİNE HOSGELDİNİZ---");
-=======
 
->>>>>>> ded766f1229b090f3db6c06c86ae5c2d407f400c
     print("---yapmak istediğiniz islemi seciniz---");
     print("   |toplama islemi icin a basiniz.");
     print("   |cikarma islemi icin b basiniz.");
@@ -31,7 +22,6 @@ void main(List<String> args) {
      print("seciminizi giriniz.");
      String? w = stdin.readLineSync()!;
 
-<<<<<<< HEAD
      islem (w);}
 
   islem ( String x ){
@@ -40,23 +30,10 @@ void main(List<String> args) {
   double deger1 = double.parse(stdin.readLineSync()!);
   print("2.sayiyi giriniz.");
   double deger2 = double.parse(stdin.readLineSync()!);
-=======
-     islem (w);
-
-}
-
- void  islem (String x ){
-  //print("seciminizi giriniz.");
-  print("1.sayiyi giriniz.");
-  double sayi1 = double.parse(stdin.readLineSync()!);
-  print("2.sayiyi giriniz.");
-  double sayi2 = double.parse(stdin.readLineSync()!);
->>>>>>> ded766f1229b090f3db6c06c86ae5c2d407f400c
 
 
   switch (x) {
     case "a":
-<<<<<<< HEAD
        toplama(deger1,deger2);
        break;
 
@@ -70,21 +47,6 @@ void main(List<String> args) {
 
     case "d":
        bolme(deger1,deger2);
-=======
-       toplama(sayi1,sayi2);
-       break;
-
-    case "b":
-       cikarma(sayi1,sayi2,sayi3: );
-       break;
-
-    case "c":
-       carpma(sayi1,sayi2) ;
-       break;
-
-    case "d":
-       bolme(sayi1,sayi2);
->>>>>>> ded766f1229b090f3db6c06c86ae5c2d407f400c
        break;
      default:
      print("gecersiz bi islem yaptiniz.");
@@ -92,9 +54,9 @@ void main(List<String> args) {
 
    }
 }
-<<<<<<< HEAD
 
  toplama (double sayi1 , double sayi2, [double opsiyonel_sayi = 0]){
+
  print("girdiginiz sayilarin toplami ${sayi1+sayi2}");
 }
 
@@ -103,6 +65,7 @@ void main(List<String> args) {
 }
 
  carpma({ required double sayi1 ,required sayi2} ){
+  print("$sayi1 * $sayi2 = ${sayi1*sayi2}");
   print("girdiginiz sayilarin carpimi ${sayi1*sayi2}");
 }
 
@@ -110,20 +73,5 @@ void main(List<String> args) {
     if(sayi2==0){
       print("HATA.\n");
     }else
-=======
-void toplama (double sayi1 , double sayi2){
- print("girdiginiz sayilarin toplami ${sayi1+sayi2}");
-}
-
-void cikarma({double sayi1 , double sayi2, double sayi3=0}){
-  print("girdiginiz sayilarin farki ${sayi1-sayi2}");
-}
-
- void carpma(double sayi1 ,double sayi2){
-  print("girdiginiz sayilarin carpimi ${sayi1*sayi2}");
-}
-
- void bolme(double sayi1,double sayi2){
->>>>>>> ded766f1229b090f3db6c06c86ae5c2d407f400c
   print("girdiginiz sayilarin bolumu ${sayi1/sayi2}");
 }
