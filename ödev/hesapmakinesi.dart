4bf816a255ad493485621e57c8717f50f3e0ac56
iremgaltun <21060625@stu.omu.edu.tr>
2023-10-26 21:09:23 +0300

    guncellme

--- ödev/hesapmakinesi.dart
+++ ödev/hesapmakinesi.dart
@@ -1,13 +1,17 @@
 
-import 'dart:ffi';
 import 'dart:io';
 
 
 
 void main(List<String> args) {
 
+ while(true)
+    anasayfa();
 
+}
 
+  anasayfa(){
+    print("---HESAP MAKİNESİNE HOSGELDİNİZ---");
     print("---yapmak istediğiniz islemi seciniz---");
     print("   |toplama islemi icin a basiniz.");
     print("   |cikarma islemi icin b basiniz.");
@@ -17,33 +21,31 @@ void main(List<String> args) {
      print("seciminizi giriniz.");
      String? w = stdin.readLineSync()!;
 
-     islem (w);
+     islem (w);}
 
-}
+  islem ( String x ){
 
- void  islem (String x ){
-  //print("seciminizi giriniz.");
   print("1.sayiyi giriniz.");
-  double sayi1 = double.parse(stdin.readLineSync()!);
+  double deger1 = double.parse(stdin.readLineSync()!);
   print("2.sayiyi giriniz.");
-  double sayi2 = double.parse(stdin.readLineSync()!);
+  double deger2 = double.parse(stdin.readLineSync()!);
 
 
   switch (x) {
     case "a":
-       toplama(sayi1,sayi2);
+       toplama(deger1,deger2);
        break;
 
     case "b":
-       cikarma(sayi1,sayi2,sayi3: );
+       cikarma(deger1,deger2 );
        break;
 
     case "c":
-       carpma(sayi1,sayi2) ;
+       carpma(sayi1:deger1,sayi2:deger2) ;
        break;
 
     case "d":
-       bolme(sayi1,sayi2);
+       bolme(deger1,deger2);
        break;
      default:
      print("gecersiz bi islem yaptiniz.");
@@ -51,18 +53,22 @@ void main(List<String> args) {
 
    }
 }
-void toplama (double sayi1 , double sayi2){
+
+ toplama (double sayi1 , double sayi2, [double opsiyonel_sayi = 0]){
  print("girdiginiz sayilarin toplami ${sayi1+sayi2}");
 }
 
-void cikarma({double sayi1 , double sayi2, double sayi3=0}){
+ cikarma(double sayi1 , double sayi2, [double opsiyonel_sayi = 0]){
   print("girdiginiz sayilarin farki ${sayi1-sayi2}");
 }
 
- void carpma(double sayi1 ,double sayi2){
+ carpma({ required double sayi1 ,required sayi2} ){
   print("girdiginiz sayilarin carpimi ${sayi1*sayi2}");
 }
 
- void bolme(double sayi1,double sayi2){
+  bolme(double sayi1,double sayi2, [double opsiyonel_sayi = 1]){
+    if(sayi2==0){
+      print("HATA.\n");
+    }else
   print("girdiginiz sayilarin bolumu ${sayi1/sayi2}");
 }
