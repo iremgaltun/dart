e7a9b96459657f41afff8d9a88498b59eb91d4dd
iremgaltun <21060625@stu.omu.edu.tr>
2023-10-26 18:49:28 +0300

    first commit

---
+++ ödev/hesapmakinesi.dart
@@ -0,0 +1,66 @@
+ /*import 'dart:io';
+
+void main(List<String> args) {
+
+    print("--------------------hesap makinesi--------------------");
+
+    print("   |toplama islemi icin 1'e basiniz.");
+    print("   |cikarma islemi icin 2'ye basiniz.");
+    print("   |carpma islemi icin 3'e basiniz.");
+    print("   |bolme islemi icin 4'e basiniz.");
+
+
+
+     print("lütfen bi sayi giriniz.");
+     String sayi1 = double.parse(stdin.readLineSync());
+     print("lütfen bi sayi giriniz.");
+     String sayi2 = double.parse(stdin.readLineSync());
+     print("lutfen yapmak istediginiz islemi secin.");
+     String? islem = stdin.readLineSync();
+
+
+
+
+
+
+
+   switch (islem) {
+    case "1":
+       toplama(int sayi1 , int sayi2);
+       break;
+
+    case "2":
+       cikarma(int sayi1 , int sayi2);
+       break;
+
+    case "3":
+       carpma(int sayi1 , int sayi2);
+       break;
+
+    case "4":
+       bolme(int sayi1 , int sayi2);
+       break;
+     default:
+     print("gecersiz bi islem yaptiniz.");
+
+
+   }
+
+}
+
+ toplama (int sayi1 , int sayi2){
+ print("girdiğginiz sayilarin toplami ${sayi1+sayi2}");
+}
+
+cikarma(int sayi1 , int sayi2){
+  print("girdiginiz sayilarin farki ${sayi1-sayi2}");
+}
+
+ carpma(int sayi1 ,int sayi2){
+  print("girdiginiz sayilarin carpimi ${sayi1*sayi2}");
+}
+
+ bolme(double sayi1,double sayi2){
+  print("girdiginiz sayilarin bolumu ${sayi1/sayi2}");
+}
+*/
